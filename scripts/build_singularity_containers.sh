71356d4d4a9fb532c2b14a8bc83c720894a10fa4
Matt Bull <matthew.bull@wales.nhs.uk>
2020-03-16 18:45:23 +0000

    Fix container build script so that it makes both containers

--- scripts/build_singularity_containers.sh
+++ scripts/build_singularity_containers.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BASEDIR=$(dirname $SCRIPTPATH)
 
-CONTAINERS=(artic-ncov2019-medaka artic-ncov2019-nanopolish)
+CONTAINERS="artic-ncov2019-medaka artic-ncov2019-nanopolish"
 
 for CONTAINER in ${CONTAINERS}; do 
     sudo singularity build ${BASEDIR}/${CONTAINER}.sif ${BASEDIR}/Singularity.${CONTAINER}
