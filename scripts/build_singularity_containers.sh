4c7f49f0bb7f5ae029e2a2968176d3cc8de55f1f
Matt Bull <matthew.bull@wales.nhs.uk>
2020-04-26 15:01:28 +0000

    Update containers to pull unified conda env from artic-ncov2019

--- scripts/build_singularity_containers.sh
+++ scripts/build_singularity_containers.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BASEDIR=$(dirname $SCRIPTPATH)
 
-CONTAINERS="artic-ncov2019-medaka artic-ncov2019-nanopolish artic-ncov2019-illumina"
+CONTAINERS="artic-ncov2019-nanopore artic-ncov2019-illumina"
 
 for CONTAINER in ${CONTAINERS}; do 
     sudo singularity build ${BASEDIR}/${CONTAINER}.sif ${BASEDIR}/Singularity.${CONTAINER}
