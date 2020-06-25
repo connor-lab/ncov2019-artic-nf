85b8a6977422211ac8a042f62b01fdf03479b161
Matt Bull <bullmj2@gmail.com>
2020-06-25 07:21:32 +0000

    Update qc.py to reflect changed standards

--- bin/qc.py
+++ bin/qc.py
@@ -137,8 +137,7 @@ def go(args):
         largest_N_gap = get_largest_N_gap(fasta)
 
     	# QC PASS / FAIL
-        if largest_N_gap >= 10000 or pct_covered_bases >= 85.0:
-            if pct_N_bases < 50.0:
+        if largest_N_gap >= 10000 or pct_N_bases < 50.0:
                 qc_pass = "TRUE"
 
 
