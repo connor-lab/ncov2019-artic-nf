d0630f27d27b1e3dfcf189b91a9775fbbfe690a1
ctr26 <ctr26@ebi.ac.uk>
2020-09-12 16:55:49 +0100

    Fixed docker switcher in conf/docker.config

--- run_ont.sh
+++ run_ont.sh
@@ -1,1 +1,5 @@
-nextflow run main.nf -profile docker --illumina --prefix "ctr26" --basecalled_fastq data --sequencing_summary sequencing_summary.txt --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
+# nextflow run main.nf -profile docker --illumina --prefix "ctr26" --directory data --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
+
+nextflow run main.nf -profile docker --illumina --prefix "ena" --directory illumina --schemeRepoURL "https://github.com/ctr26/primer-schemes"
+
+nextflow run main.nf -profile docker --nanopolish --prefix "ctr26" --basecalled_fastq data --sequencing_summary sequencing_summary.txt --schemeRepoURL "https://github.com/ctr26/primer-schemes"
