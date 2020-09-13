947b9c1555ddc09a2212522ec1f32a3f51d3218f
ctr26 <ctr26@ebi.ac.uk>
2020-09-13 08:08:24 +0100

    fixing run_*.sh

--- run_illumina.sh
+++ run_illumina.sh
@@ -1,4 +1,12 @@
-nextflow run connor-lab/ncov2019-artic-nf -profile docker --nanopolish --prefix "ctr26" --basecalled_fastq data --sequencing_summary sequencing_summary.txt --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
+# nextflow run connor-lab/ncov2019-artic-nf -profile docker --nanopolish --prefix "ctr26" --basecalled_fastq data --sequencing_summary sequencing_summary.txt --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
 
 
-nextflow run main.nf -profile docker --illumina --prefix "ctr26" --directory data --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
+# nextflow run main.nf -profile docker --illumina --prefix "ctr26" --directory data --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
+
+
+FILENAME = "$1"
+
+INPUT = "/data/${FILENAME}_input/"
+OUTDIR="results"
+
+nextflow run main.nf -profile docker --illumina --prefix "ena" --directory $INPUT --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
--- run_ont.sh
+++ run_ont.sh
@@ -1,5 +1,10 @@
-# nextflow run main.nf -profile docker --illumina --prefix "ctr26" --directory data --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
+# # nextflow run main.nf -profile docker --illumina --prefix "ctr26" --directory data --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
 
-nextflow run main.nf -profile docker --illumina --prefix "ena" --directory illumina --schemeRepoURL "https://github.com/ctr26/primer-schemes"
+# nextflow run main.nf -profile docker --illumina --prefix "ena" --directory illumina --schemeRepoURL "https://github.com/ctr26/primer-schemes"
 
-nextflow run main.nf -profile docker --nanopolish --prefix "ctr26" --basecalled_fastq data --sequencing_summary sequencing_summary.txt --schemeRepoURL "https://github.com/ctr26/primer-schemes"
+FILENAME = "$1"
+
+INPUT = "/data/${FILENAME}_input/"
+OUTDIR="results"
+
+nextflow run ncov2019-artic-nf/main.nf -profile docker --nanopolish --prefix "ena" --basecalled_fastq $INPUT --fast5-directory $INPUT --sequencing_summary sequencing_summary.txt --schemeRepoURL "https://github.com/ctr26/primer-schemes"
