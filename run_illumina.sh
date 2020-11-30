c0b1275e422ca6d4b46d7c3e45a4df9476e4b9e4
ctr26 <ctr26@ebi.ac.uk>
2020-11-30 10:47:13 +0000

    Making it ready for PR

--- run_illumina.sh
+++
@@ -1,9 +0,0 @@
-# nextflow run connor-lab/ncov2019-artic-nf -profile docker --nanopolish --prefix "ctr26" --basecalled_fastq data --sequencing_summary sequencing_summary.txt --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
-
-
-# nextflow run main.nf -profile docker --illumina --prefix "ctr26" --directory data --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
-
-
-INPUT = "$1"
-
-nextflow run main.nf -profile k8s --illumina --prefix "ena" --directory $INPUT
--- run_ont.sh
+++
@@ -1,9 +0,0 @@
-# # nextflow run main.nf -profile docker --illumina --prefix "ctr26" --directory data --schemeRepoURL "--recurse-submodules https://github.com/artic-network/artic-ncov2019"
-
-# nextflow run main.nf -profile docker --illumina --prefix "ena" --directory illumina --schemeRepoURL "https://github.com/ctr26/primer-schemes"
-
-INPUT = "$1"
-
-nextflow run main.nf -profile k8s --medaka --prefix "ena" --basecalled_fastq $INPUT
-
-# nextflow run main.nf -profile docker --medaka --prefix "ena" --basecalled_fastq $INPUT
