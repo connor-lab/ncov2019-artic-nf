d8c203c26ffbd5e2900c31333217a3ecbd352767
Jyotirmoy Das <jyotirmoy21@gmail.com>
2021-03-30 14:35:11 +0200

    artic-pangloin-conda-nextflow

---
+++ ncov2019-artic-nf/.github/scripts/install_conda.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eo pipefail
+
+echo Install Conda .. >> artifacts/test_artifact.log
+
+export PATH=/opt/conda/bin:$PATH
+sudo apt-get update --fix-missing && sudo apt-get install -y wget bzip2 ca-certificates \
+    libglib2.0-0 libxext6 libsm6 libxrender1 \
+    git mercurial subversion
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
+    /bin/bash ~/miniconda.sh -b -p /opt/conda && \
+    rm ~/miniconda.sh && \
+    ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
+    echo "conda activate base" >> ~/.bashrc
+
+conda update -n base conda
+
