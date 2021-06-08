931b8a2c8d5b6c5f74030335b98e53d83e7bacf3
nicholas sanderson <nicholasdsanderson@gmail.com>
2021-06-08 14:28:19 +0000

    fixed error in merging reports

--- bin/makeReport.py
+++ bin/makeReport.py
@@ -11,6 +11,6 @@ nextclade['sample name']=sys.argv[1]
 aln2type['sample name']=sys.argv[1]
 
 df=pango.merge(nextclade, on='sample name', how='left', suffixes=("_pango","_nextclade"))
-df=df.merge(aln2type, left_on='sample name', right_on='sample_id', how='left', suffixes=(None,"_aln2type"))
+df=df.merge(aln2type, on='sample name', how='left', suffixes=(None,"_aln2type"))
 
 df.to_csv('{0}_report.tsv'.format(sys.argv[1]), sep='\t', index=False)
