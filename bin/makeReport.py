d43104f84bbfcc017ee5542a1937b7316ea7bbc0
nicholas sanderson <nicholasdsanderson@gmail.com>
2021-06-08 11:36:49 +0000

    adjusted report

--- bin/makeReport.py
+++ bin/makeReport.py
@@ -6,8 +6,11 @@ pango=pd.read_csv('pango.csv')
 nextclade=pd.read_csv('nextclade.tsv', sep='\t')
 aln2type=pd.read_csv('aln2type.csv')
 
-df=pango.merge(nextclade, left_on='taxon',right_on='seqName', how='left', suffixes=("_pango","_nextclade"))
-df=df.merge(aln2type, left_on='taxon', right_on='sample_id', how='left', suffixes=(None,"_aln2type"))
-df['sample name']=sys.argv[1]
+pango['sample name']=sys.argv[1]
+nextclade['sample name']=sys.argv[1]
+aln2type['sample name']=sys.argv[1]
+
+df=pango.merge(nextclade, on='sample name', how='left', suffixes=("_pango","_nextclade"))
+df=df.merge(aln2type, left_on='sample name', right_on='sample_id', how='left', suffixes=(None,"_aln2type"))
 
 df.to_csv('{0}_report.tsv'.format(sys.argv[1]), sep='\t', index=False)
