644a7ed36ec5da6015fff54ad859b29d8600d3f9
Anthony Underwood <au3@sanger.ac.uk>
2021-06-28 10:59:34 +0000

    cd into correct dir

--- .github/scripts/install_singularity.sh
+++ .github/scripts/install_singularity.sh
@@ -24,7 +24,7 @@ export VERSION=3.5.3
 echo Install Singularity version $VERSION .. >> artifacts/test_artifact.log
 wget https://codeload.github.com/sylabs/singularity/tar.gz/refs/tags/v${VERSION} -O singularity-${VERSION}.tar.gz
 tar -xzf singularity-${VERSION}.tar.gz
-cd singularity
+cd singularity-${VERSION}
 ./mconfig
 make -C builddir
 sudo make -C builddir install
