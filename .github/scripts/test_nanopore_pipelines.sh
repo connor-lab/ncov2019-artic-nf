23ab57fa462137a66aa0fdbec0048b912e351790
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2021-10-25 16:33:58 +0200

    Test

--- .github/scripts/test_nanopore_pipelines.sh
+++ .github/scripts/test_nanopore_pipelines.sh
@@ -18,11 +18,11 @@ rm -rf results && rm -rf work && rm -rf .nextflow*
 echo "Nextflow run current PR in --nanopolish mode (barcodes).." >> artifacts/test_artifact.log
 NXF_VER=20.03.0-edge nextflow run ./main.nf \
        -profile singularity \
-       --nanopolish \
-       --sequencing_summary .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/sequencing_summary_FAK72834_298b7829.txt \
+       --nanopolish --prefix "test_nanopore" \
        --basecalled_fastq .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fastq_pass/ \
        --fast5_pass .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fast5_pass/ \
-       --prefix 20200311_1427_X1_FAK72834_a3787181 
+       --sequencing_summary .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/sequencing_summary_FAK72834_298b7829.txt 
+       #--prefix 20200311_1427_X1_FAK72834_a3787181 
 cp .nextflow.log artifacts/nanopolish_barcodes.nextflow.log
 rm -rf results && rm -rf work && rm -rf .nextflow*
 
