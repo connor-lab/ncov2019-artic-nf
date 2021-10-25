3c6e4a3192a4b11b0f4a9a2eaf03eed649acf0a5
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2021-10-25 09:12:11 +0200

    Test without /Users/sofia.stamouli/Documents/microcg/gms-artic/.github/scripts

--- .github/scripts/test_nanopore_pipelines.sh
+++ .github/scripts/test_nanopore_pipelines.sh
@@ -9,9 +9,9 @@ echo "Nextflow run current PR in --nanopolish mode (no barcodes).." >> artifacts
 NXF_VER=20.03.0-edge nextflow run ./main.nf \
        -profile singularity \
        --nanopolish \
-       --sequencing_summary $PWD/.github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/sequencing_summary_FAK72834_298b7829.txt \
-       --basecalled_fastq $PWD/.github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fastq_pass/ \
-       --fast5_pass $PWD/.github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fast5_pass/ \
+       --sequencing_summary .github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/sequencing_summary_FAK72834_298b7829.txt \
+       --basecalled_fastq .github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fastq_pass/ \
+       --fast5_pass .github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fast5_pass/ \
        --prefix 20200311_1427_X4_FAK72834_a3787181 
 cp .nextflow.log artifacts/nanopolish.nextflow.log
 rm -rf results && rm -rf work && rm -rf .nextflow*
@@ -20,9 +20,9 @@ echo "Nextflow run current PR in --nanopolish mode (barcodes).." >> artifacts/te
 NXF_VER=20.03.0-edge nextflow run ./main.nf \
        -profile singularity \
        --nanopolish \
-       --sequencing_summary $PWD/.github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/sequencing_summary_FAK72834_298b7829.txt \
-       --basecalled_fastq $PWD/.github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fastq_pass/ \
-       --fast5_pass $PWD/.github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fast5_pass/ \
+       --sequencing_summary .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/sequencing_summary_FAK72834_298b7829.txt \
+       --basecalled_fastq .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fastq_pass/ \
+       --fast5_pass .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fast5_pass/ \
        --prefix 20200311_1427_X1_FAK72834_a3787181 
 cp .nextflow.log artifacts/nanopolish_barcodes.nextflow.log
 rm -rf results && rm -rf work && rm -rf .nextflow*
@@ -31,7 +31,7 @@ echo "Nextflow run current PR in --medaka mode (no barcodes).." >> artifacts/tes
 NXF_VER=20.03.0-edge nextflow run ./main.nf \
        -profile singularity \
        --medaka \
-       --basecalled_fastq $PWD/.github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fastq_pass/ \
+       --basecalled_fastq .github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fastq_pass/ \
        --prefix 20200311_1427_X4_FAK72834_a3787181
 cp .nextflow.log artifacts/medaka.nextflow.log
 rm -rf results && rm -rf work && rm -rf .nextflow*
@@ -40,7 +40,7 @@ echo "Nextflow run current PR in --medaka mode (barcodes).." >> artifacts/test_a
 NXF_VER=20.03.0-edge nextflow run ./main.nf \
        -profile singularity \
        --medaka \
-       --basecalled_fastq $PWD/.github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fastq_pass/ \
+       --basecalled_fastq .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fastq_pass/ \
        --prefix 20200311_1427_X1_FAK72834_a3787181
 
 cp .nextflow.log artifacts/medaka_barcodes.nextflow.log
