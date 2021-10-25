9e67ba78d0e8547611a4118c09954127ff4dc498
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2021-10-25 10:15:10 +0200

    Test pathway

--- .github/scripts/test_nanopore_pipelines.sh
+++ .github/scripts/test_nanopore_pipelines.sh
@@ -5,13 +5,13 @@ export PATH=/opt/conda/bin:$PATH
 # run current pull request code
 singularity --version
 # write test log as github Action artifact
-#echo "Nextflow run current PR in --nanopolish mode (no barcodes).." >> artifacts/test_artifact.log
+echo "Nextflow run current PR in --nanopolish mode (no barcodes).." >> artifacts/test_artifact.log
 NXF_VER=20.03.0-edge nextflow run ./main.nf \
        -profile singularity \
        --nanopolish \
-       --sequencing_summary .github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/sequencing_summary_FAK72834_298b7829.txt \
-       --basecalled_fastq .github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fastq_pass/ \
-       --fast5_pass .github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fast5_pass/ \
+       --sequencing_summary /home/test/sequencing_summary_FAK72834_298b7829.txt \
+       --basecalled_fastq /home/test/fastq_pass/ \
+       --fast5_pass /home/test/fast5_pass/ \
        --prefix 20200311_1427_X4_FAK72834_a3787181 
 cp .nextflow.log artifacts/nanopolish.nextflow.log
 rm -rf results && rm -rf work && rm -rf .nextflow*
