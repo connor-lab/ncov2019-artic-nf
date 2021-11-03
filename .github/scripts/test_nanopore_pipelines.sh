a0a9d8aa4a71762e5abd54ca97c70190d803c3f3
Sofia Stamouli <91951607+sofstam@users.noreply.github.com>
2021-11-03 14:11:11 +0100

    Update nextflow version

--- .github/scripts/test_nanopore_pipelines.sh
+++ .github/scripts/test_nanopore_pipelines.sh
@@ -7,7 +7,7 @@ export PATH=/opt/conda/bin:$PATH
 singularity --version
 # write test log as github Action artifact
 echo "Nextflow run current PR in --nanopolish mode (no barcodes).." >> artifacts/test_artifact.log
-NXF_VER=20.03.0-edge nextflow run main.nf \
+NXF_VER=21.04.0 nextflow run main.nf \
        -profile singularity \
        --nanopolish --prefix "test_nanopore" \
        --basecalled_fastq .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fastq_pass/ \
@@ -17,7 +17,7 @@ cp .nextflow.log artifacts/nanopolish.nextflow.log
 rm -rf results && rm -rf work && rm -rf .nextflow*
 
 echo "Nextflow run current PR in --nanopolish mode (barcodes).." >> artifacts/test_artifact.log
-NXF_VER=20.03.0-edge nextflow run main.nf \
+NXF_VER=21.04.0 nextflow run main.nf \
        -profile singularity \
        --nanopolish --prefix "20200311_1427_X1_FAK72834_a3787181" \
        --basecalled_fastq .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fastq_pass/ \
@@ -27,7 +27,7 @@ cp .nextflow.log artifacts/nanopolish_barcodes.nextflow.log
 rm -rf results && rm -rf work && rm -rf .nextflow*
 
 echo "Nextflow run current PR in --medaka mode (no barcodes).." >> artifacts/test_artifact.log
-NXF_VER=20.03.0-edge nextflow run main.nf \
+NXF_VER=21.04.0 nextflow run main.nf \
        -profile singularity \
        --medaka \
        --basecalled_fastq .github/data/nanopore/20200311_1427_X4_FAK72834_a3787181/fastq_pass/ \
@@ -36,7 +36,7 @@ cp .nextflow.log artifacts/medaka.nextflow.log
 rm -rf results && rm -rf work && rm -rf .nextflow*
 
 echo "Nextflow run current PR in --medaka mode (barcodes).." >> artifacts/test_artifact.log
-NXF_VER=20.03.0-edge nextflow run main.nf \
+NXF_VER=21.04.0 nextflow run main.nf \
        -profile singularity \
        --medaka  \
        --basecalled_fastq .github/data/nanopore/20200311_1427_X1_FAK72834_a3787181/fastq_pass/ \
