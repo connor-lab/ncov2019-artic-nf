6d493197df7af0c3c0385f207e430703a0ca10bc
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2021-11-04 16:50:57 +0100

    Use tag of latest release

--- .github/scripts/test_PR_against_release.sh
+++ .github/scripts/test_PR_against_release.sh
@@ -17,9 +17,9 @@ cp -r results results_singularity_profile
 cp -r work work_singularity_profile
 
 # run tests against previous previous_release to compare outputs 
-git clone https://github.com/connor-lab/ncov2019-artic-nf.git previous_release 
+git clone https://github.com/genomic-medicine-sweden/gms-artic.git previous_release 
 cd previous_release
-git checkout tags/v1.1.1 
+git checkout tags/v1.4.0
 
 # the github runner only has 2 cpus available, so replace for that commit required:
 sed -i s'/cpus = 4/cpus = 2/'g conf/resources.config
