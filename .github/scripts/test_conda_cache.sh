ade828014230c9d87e4f47618f0c0936e9e6859b
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2021-11-22 11:47:31 +0100

    Exclude test_conda_cache.sh

--- .github/scripts/test_conda_cache.sh
+++ .github/scripts/test_conda_cache.sh
@@ -1,24 +1,22 @@
-#!/bin/bash
-set -eo pipefail
-export PATH=/opt/conda/bin:$PATH
+##!/bin/bash
+#set -eo pipefail
+#export PATH=/opt/conda/bin:$PATH
 
-mkdir conda_cache_dir
+#mkdir conda_cache_dir
 
-# first NF run will create the conda env in the cache dir
-echo run pipeline with conda --cache to create cache.. >> artifacts/test_artifact.log
-export REPO=$PWD
-##echo REPO=$REPO >> artifacts/test_artifact.log
+## first NF run will create the conda env in the cache dir
+#echo run pipeline with conda --cache to create cache.. >> artifacts/test_artifact.log
+#export REPO=$PWD
+#echo REPO=$REPO >> artifacts/test_artifact.log
 #cd ..
 #echo PWD=$PWD >> $REPO/artifacts/test_artifact.log
-#NXF_VER=21.04.0 nextflow run $REPO \
-NXF_VER=21.04.0 nextflow run main.nf \
-       -profile conda \  
+#NXF_VER=20.03.0-edge nextflow run $REPO \
+#       -profile conda \
 #       --cache $REPO/conda_cache_dir \
-#       #--directory $REPO/.github/data/fastqs/ \
-       --directory .github/data/fastqs/ \
-       --illumina \
-       --prefix "test"
-cp .nextflow.log artifacts/cache_creation.conda.profile.nextflow.log
+#       --directory $REPO/.github/data/fastqs/ \
+#       --illumina \
+#       --prefix test
+#cp .nextflow.log $REPO/artifacts/cache_creation.conda.profile.nextflow.log
 
 #cat .nextflow.log | grep 'Conda create complete env=/home/runner/work/ncov2019-artic-nf/ncov2019-artic-nf/environments/illumina/environment.yml path=/home/runner/work/ncov2019-artic-nf/ncov2019-artic-nf/conda_cache_dir/artic-ncov2019-illumina-' \
 #    && echo "Conda env created in cache dir" >> $REPO/artifacts/test_artifact.log \
@@ -27,14 +25,12 @@ cp .nextflow.log artifacts/cache_creation.conda.profile.nextflow.log
 #rm -rf results && rm -rf work && rm -rf .nextflow*
 ## second NF run will use the conda env created in the previous run
 #echo re-run pipeline with conda --cache.. >> $REPO/artifacts/test_artifact.log
-#NXF_VER=21.04.0 nextflow run $REPO \
-##NXF_VER=21.04.0 nextflow run main.nf \
+#NXF_VER=20.03.0-edge nextflow run $REPO \
 #       -profile conda \
 #       --cache $REPO/conda_cache_dir \
-#       #--directory $REPO/.github/data/fastqs/ \
-#       --directory .github/data/fastqs/ \
+#       --directory $REPO/.github/data/fastqs/ \
 #       --illumina \
-#       --prefix "test"
+#       --prefix test
 #cp .nextflow.log $REPO/artifacts/cache_use.conda.profile.nextflow.log
 
 #cat .nextflow.log | grep 'Conda found local env for environment=/home/runner/work/ncov2019-artic-nf/ncov2019-artic-nf/environments/illumina/environment.yml; path=/home/runner/work/ncov2019-artic-nf/ncov2019-artic-nf/conda_cache_dir/artic-ncov2019-illumina-' \
